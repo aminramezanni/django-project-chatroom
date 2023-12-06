a4ff329e503356ab6b238de0e4565221d4b2863b
amin ramezani <aminramezanni@gmail.com>
2023-12-07 03:38:33 +0400

    create full CRUD and search

--- home/views.py
+++ home/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {"rooms": rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    rooms = Room.objects.filter(Q(topic__name__icontains=q) |
+    Q(name__icontains=q) |
+    Q(description__icontains=q)
+    )
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+    context = {"rooms": rooms, 'topics':topics, 'room_count': room_count}
     return render(request, "home/home.html", context)
 
 
@@ -24,3 +32,25 @@ def create_room(request):
             return redirect("home")
     context = {"form": form}
     return render(request, "home/room_form.html", context)
+
+
+def update_room(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    context = {"form": form}
+    return render(request, "home/room_form.html", context)
+
+
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=pk)
+
+    if request.method == "POST":
+        room.delete()
+        return redirect('home')
+    return render(request, 'home/delete.html',{'obj':room})
