be8efb4d6dee8f85f8942c4ffbf5998d551ad733
amin ramezani <aminramezanni@gmail.com>
2023-12-07 17:39:33 +0400

    create login and logout page

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
+    path('login/',views.login_page, name='login'),
+    path('logout/',views.logout_page, name='logout'),
     path('room/<str:pk>/',views.room, name= 'room'),
     path('create-room/', views.create_room, name='create-room'),
     path('update-room/<str:pk>/', views.update_room, name='update-room'),
--- home/views.py
+++ home/views.py
@@ -1,7 +1,39 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from django.db.models import Q
+from django.contrib.auth.models import User
 from .models import Room, Topic
 from .forms import RoomForm
+
+
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+            
+        user = authenticate(username= username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'username or password is wrong !')
+            
+    context = {}
+    return render(request, 'home/login.html', context)
+
+
+
+def logout_page(request):
+    logout(request)
+    return redirect('home')
+
 
 
 def home(request):
