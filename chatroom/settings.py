292cc79fd2defee7aaa790fdffcb8d24b1943630
amin ramezani <aminramezanni@gmail.com>
2023-12-10 03:41:22 +0400

    delete database and create custom user model

--- chatroom/settings.py
+++ chatroom/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'home.apps.HomeConfig',
 ]
 
+AUTH_USER_MODEL = 'home.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- home/views.py
+++ home/views.py
@@ -5,8 +5,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from .models import Room, Topic, Message
+from .models import Room, Topic, Message, User
 from .forms import RoomForm, UserForm
 
 
